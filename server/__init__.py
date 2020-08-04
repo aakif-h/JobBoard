cacf7227ad24dada611480e460d28f2234a3cb69
aakif-h <aakifhussaini@gmail.com>
2020-08-03 17:59:39 -0700

    Added login and job-app page functionality

--- server/__init__.py
+++ server/__init__.py
@@ -3,8 +3,24 @@ from server.Kit import *
 
 pages = Blueprint('pages', __name__)
 @pages.route('/', methods=['GET'])
+@pages.route('/index.html', methods=['GET','POST'])
 def pre_ship():
     try:
         return render_template('index.html')
     except:
         return jsonify(**{'message': 'Unexpected Error'}), ErrorCode_ServerError
+
+@pages.route('/job-view.html', methods=['GET'])
+def go_to_job_view():
+    try:
+        return render_template('job-view.html')
+    except:
+        return jsonify(**{'message': 'Unexpected Error'}), ErrorCode_ServerError
+
+@pages.route('/login.html', methods=['GET'])
+def go_to_login():
+    try:
+        return render_template('login.html')
+    except:
+        return jsonify(**{'message': 'Unexpected Error'}), ErrorCode_ServerError
+
